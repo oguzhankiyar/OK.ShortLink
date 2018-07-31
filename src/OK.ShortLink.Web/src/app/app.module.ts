210727be1145e1d37b03b8a3d2f073a2e31f8a41
Oğuzhan Kiyar <oguzhan.kiyar@outlook.com>
2018-08-01 02:43:52 +0300

    Services added to module providers.

--- src/OK.ShortLink.Web/src/app/app.module.ts
+++ src/OK.ShortLink.Web/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthService } from './_services/auth.service';
 import { StorageService } from './_services/storage.service';
 import { UserService } from './_services/user.service';
+import { LinkService } from './_services/link.service';
+import { VisitorService } from './_services/visitor.service';
 import { ApiInterceptor } from './_helpers/api.interceptor';
 import { AppComponent } from './app.component';
 
@@ -14,6 +16,8 @@ import { AppComponent } from './app.component';
     StorageService,
     AuthService,
     UserService,
+    LinkService,
+    VisitorService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ApiInterceptor,
