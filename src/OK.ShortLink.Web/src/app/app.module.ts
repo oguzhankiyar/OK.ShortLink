12e8d97736a0314860482efd809a9612efb0bc51
Oğuzhan Kiyar <oguzhan.kiyar@outlook.com>
2018-07-31 21:54:55 +0300

    Added auth interceptor for auth-required requests.

--- src/OK.ShortLink.Web/src/app/app.module.ts
+++ src/OK.ShortLink.Web/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthService } from './_services/auth.service';
 import { StorageService } from './_services/storage.service';
+import { AuthInterceptor } from './_helpers/auth.interceptor';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule],
-  providers: [StorageService, AuthService],
+  providers: [
+    StorageService,
+    AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
